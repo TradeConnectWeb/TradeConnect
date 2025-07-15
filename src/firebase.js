60ef7bef4ca772dcd6535b9b1b6db6a47c70e472
kaikai <122327caratao@gmail.com>
2025-07-15 16:28:31 +0800

    Fix: Finalized server with WebSocket, dotenv, express setup

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-// src/firebase.js
-import { initializeApp } from "firebase/app";
-
-export async function getFirebaseApp() {
-  const response = await fetch("/firebase-config");
-  const config = await response.json();
-  const app = initializeApp(config);
-  return app;
-}
